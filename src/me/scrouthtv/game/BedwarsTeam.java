417846c896577474f226685e20b16db129fdd4ab
Lenni <lennivh24@gmail.com>
2021-03-25 16:20:17 +0000

    order of team colors

--- src/me/scrouthtv/game/BedwarsTeam.java
+++ src/me/scrouthtv/game/BedwarsTeam.java
@@ -1,5 +1,14 @@
 package me.scrouthtv.game;
 
+import org.bukkit.DyeColor;
+
 public class BedwarsTeam {
 	private BedwarsPlayer[] players;
+	
+	public static final DyeColor[] colorOrder = {
+			DyeColor.BLUE, DyeColor.RED,
+			DyeColor.YELLOW, DyeColor.PINK,
+			DyeColor.BLACK, DyeColor.GREEN, DyeColor.ORANGE, DyeColor.WHITE,
+			DyeColor.LIME, DyeColor.LIGHT_BLUE, DyeColor.MAGENTA, DyeColor.BROWN, DyeColor.LIGHT_GRAY, DyeColor.PURPLE, DyeColor.CYAN, DyeColor.GRAY
+	};
 }
